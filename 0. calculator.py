30356ddb1461c95506cd4aff6aedc93ab2560e19
Zaur Makharov <makharovz@gmail.com>
2025-06-30 21:22:14 +0300

    Добавил первый калькулятор

---
+++ 0. calculator.py
@@ -0,0 +1,9 @@
+print("Сегодня будем складыват!ь!!")
+num1 = input("Введите первое число: ")
+num2 = input("Введите второе число: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+print("Ответ: ")
+print(num1 + num2)
--- 4. number.py
+++ 4. number.py
@@ -1,5 +1,51 @@
-num_1 = 15
-num_2 = 10
-result = (num_1 * num_2)*2
+# Числовые типы данных
 
-print(result*2)
+num_1 = 5
+num_1 = 10
+"""
+Результатом вывода будет - 10, потому что переменная num_1 была переопределена. Интерпретатор читает код 
+последовательно: сверху вниз
+"""
+# print(num_1)
+
+# Пример со сложением:
+val_1 = 5
+val_2 = 10
+result = val_1 - val_2
+# print(result, type(result))
+
+# Пример с вычитанием:
+val_3 = 10
+val_4 = 5
+result = val_3 - val_4
+# print(result, type(result))
+
+# Пример с умножением:
+val_5 = 5
+val_6 = 5
+result = val_5 * val_6
+# print(result, type(result))
+
+# Пример с делением (с плавающей точкой):
+val_7 = 6
+val_8 = 2
+result = val_7 / val_8
+# print(result, type(result))
+
+# Пример с делением (целочисленное деление):
+val_9 = 6
+val_10 = 2
+result = val_9 // val_10
+# print(result, type(result))
+
+# Пример с уможением:
+new_1 = 2
+new_2 = 2
+result = new_1 * new_2
+# print(result, type(result))
+
+# Пример с возведением в степень:
+new_1 = 5
+new_2 = 2
+result = new_1 ** new_2
+print(result, type(result))
