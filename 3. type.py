966a4694a024e3074027572caff673cf6112d2d2
Zaur Makharov <makharovz@gmail.com>
2025-06-30 14:39:06 +0300

    Добавил файл с описанием функции type

---
+++ 3. type.py
@@ -0,0 +1,11 @@
+# Функция type позволяет узнать, к какому типу относится объект. Например:
+
+var_1 = 1                                                                       # целое число (int)
+var_2 = 1.5                                                                     # не целое (float)
+var_3 = 'Hello, World!'                                                         # строка (str)
+var_4 = False                                                                   # истина/ложь (bool)
+var_5 = [1, 2, 3, 4]                                                            # список (list)
+var_6 = {'Hello', 'World', 'Devices', 'Cars'}                                   # множества (list)
+var_7 = {'Name':'Zaur', 'Job Title':'QA Engineer', 'Company':'AFLT-Systems'}    # множества/ключ:значение (dict)
+
+print(type(var_7))
