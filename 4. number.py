094a413eda84be4f0988816bd41c3319a673186c
Zaur Makharov <makharovz@gmail.com>
2025-07-01 21:20:08 +0300

    Добавил файл '4. number.py', в котором разобрал числовые типы данных

--- 4. number.py
+++ 4. number.py
@@ -1,51 +1,99 @@
 # Числовые типы данных
 
-num_1 = 5
-num_1 = 10
+num1 = 5
+num1 = 10
 """
-Результатом вывода будет - 10, потому что переменная num_1 была переопределена. Интерпретатор читает код 
-последовательно: сверху вниз
+Результатом вывода будет - 10, потому что переменная num1 была переопределена. Интерпретатор читает код 
+последовательно: сверху вниз, слева направо
 """
-# print(num_1)
+# print(num1)
 
 # Пример со сложением:
-val_1 = 5
-val_2 = 10
-result = val_1 - val_2
+num2 = 10
+num3 = 5
+result = num2 + num3
 # print(result, type(result))
 
 # Пример с вычитанием:
-val_3 = 10
-val_4 = 5
-result = val_3 - val_4
+num4 = 10
+num5 = 5
+result = num4 - num5
 # print(result, type(result))
 
 # Пример с умножением:
-val_5 = 5
-val_6 = 5
-result = val_5 * val_6
+num6 = 5
+num7 = 5
+result = num6 * num7
 # print(result, type(result))
 
 # Пример с делением (с плавающей точкой):
-val_7 = 6
-val_8 = 2
-result = val_7 / val_8
+num8 = 6
+num9 = 2
+result = num8 / num9
 # print(result, type(result))
+
+# Пример #2 с делением (целочисленное деление):
+num10 = 6
+num11 = 2
+result = num10 / num11
+# print(int(result), type(result))
 
 # Пример с делением (целочисленное деление):
-val_9 = 6
-val_10 = 2
-result = val_9 // val_10
+num11 = 6
+num12 = 2
+result = num11 // num12
 # print(result, type(result))
 
-# Пример с уможением:
-new_1 = 2
-new_2 = 2
-result = new_1 * new_2
+# Пример с умножением:
+num13 = 2
+num14 = 2
+result = num13 * num14
 # print(result, type(result))
 
 # Пример с возведением в степень:
-new_1 = 5
-new_2 = 2
-result = new_1 ** new_2
-print(result, type(result))
+num15 = 5
+num16 = 2
+result = num15 ** num16
+# print(result, type(result))
+
+# Пример с выводом остатка при делении:
+num17 = 12
+num18 = 5
+result = num17 % num18
+# print(result, type(result))
+
+# Пример со сравнением (true/false):
+# ==    - равенство
+# !=    - неравенство
+# >     - больше
+# <     - меньше
+# >=    - больше или равно
+# <=    - меньше или равно
+num19 = 5
+num20 = 4
+result = num19 % num20
+# print(result type(result))
+
+# Пример с округлением:
+num21 = 10
+num22 = 3
+result = num21 / num22
+# print(result, type(result))
+# print(round (result), type(result))
+
+# Пример #2 с округлением:
+num23 = 10.5
+num24 = 5
+result = num23 + num24
+# print(int(result))
+# или (int) можно передать в переменной:
+# result = int(num23 + num24)
+
+# Пример #3 с округлением:
+num25 = 10.5
+num26 = 5
+result = (10.5 / 5)
+# print()
+
+# TODO: Надо разобраться с округлением при помощи round и создать отдельный файл с задачами, которые были в теме
+#  4. nubmers.py
